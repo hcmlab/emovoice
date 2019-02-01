a0952defcdcbec086321182fc4243e2162f753d4
Dominik Schiller <dominik.schiller@informatik.uni-augsburg.de>
2019-02-01 08:42:59 +0100

    addding loopback capabilites
    it is now possible to use audio-loopback to test the model with computer playback.

---
+++ do_run_loopback.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+SET ROOT=%~dp0
+
+SET PIPELINE=emovoice
+SET CONFIG=model.pipeline-config
+
+echo model:type=%1 > %CONFIG%
+echo model:chain=%2 >> %CONFIG%
+echo audio:live=true >> %CONFIG%
+echo audio:live:mic=False >> %CONFIG%
+
+%ROOT%bin\xmlpipe.exe -log ssi.log -config %CONFIG% %PIPELINE%
+
+DEL %CONFIG%
+pause
